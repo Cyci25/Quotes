08ef3cad988b85e39d84a5060f2c2b2d75663a62
Cyci25 <c2muriithi@gmail.com>
2018-10-19 15:53:16 +0300

    add a class and a component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+import { DeclareFunctionStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quote =[
+    new Quote(1,'Mohamed Ali',"Sometimes it's better to know when to quit the battle just to win the war",'Cynthia Muriithi','15th December 2018'),
+    new Quote(1,'Mohamed Ali',"Sometimes it's better to know when to quit the battle just to win the war",'Cynthia Muriithi','15th December 2018'),
+    new Quote(1,'Mohamed Ali',"Sometimes it's better to know when to quit the battle just to win the war",'Cynthia Muriithi','15th December 2018'),
+    new Quote(1,'Mohamed Ali',"Sometimes it's better to know when to quit the battle just to win the war",'Cynthia Muriithi','15th December 2018'),
+    new Quote(1,'Mohamed Ali',"Sometimes it's better to know when to quit the battle just to win the war",'Cynthia Muriithi','15th December 2018'),
+    new Quote(1,'Mohamed Ali',"Sometimes it's better to know when to quit the battle just to win the war",'Cynthia Muriithi','15th December 2018'),
+    new Quote(1,'Mohamed Ali',"Sometimes it's better to know when to quit the battle just to win the war",'Cynthia Muriithi','15th December 2018'),
+    new Quote(1,'Mohamed Ali',"Sometimes it's better to know when to quit the battle just to win the war",'Cynthia Muriithi','15th December 2018'),
+
+
+  ]
 
   constructor() { }
 
